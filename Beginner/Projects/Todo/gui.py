fcfc23d0a163a7d006723934d21b6f8bc7d41949
Eray <erayguler6757@gmail.com>
2023-10-27 22:38:54 +0300

    printing name

---
+++ Beginner/Projects/Todo/gui.py
@@ -0,0 +1,9 @@
+import functions
+import PySimpleGUI as sq
+
+label = sq.Text("Type in a to-do")
+input_box = sq.InputText(tooltip="Enter todo")
+add_button = sq.Button("Add")
+window = sq.Window("My To-Do App", layout=[[label], [input_box,add_button]])
+window.read()
+window.close()
