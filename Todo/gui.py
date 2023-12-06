135d61d94bb631548fb5f7314b92c3a3e9c56424
Eray <erayguler6757@gmail.com>
2023-12-06 21:08:12 +0300

    Create basic GUI without interactions, day 16

--- Todo/gui.py
+++ Todo/gui.py
@@ -1,37 +1,9 @@
-import functions
 import PySimpleGUI as sq
+import functions
 
 label = sq.Text("Type in a to-do")
-input_box = sq.InputText(tooltip="Enter todo", key="todo")
+input_box = sq.InputText(tooltip="Enter todo")
 add_button = sq.Button("Add")
-list_box = sq.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[45, 10])
-edit_button = sq.Button("Edit")
-window = sq.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=("Helvetica", 15))
-while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    match event:
-        case "Add":
-            todos = functions.get_todos()
-            new_todo = values["todo"] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-        case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
-
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-        case "todos":
-            window["todo"].update(value=values["todos"][0])
-        case sq.WIN_CLOSED:
-            break
+window = sq.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window.read()
 window.close()
