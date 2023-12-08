7396f31e47bab2249817f4f91a984e6e65420b45
Eray Güler <136863692+erayglr@users.noreply.github.com>
2023-12-08 12:47:17 +0300

    Delete Todo directory

--- Todo/gui.py
+++
@@ -1,9 +0,0 @@
-import PySimpleGUI as sq
-import functions
-
-label = sq.Text("Type in a to-do")
-input_box = sq.InputText(tooltip="Enter todo")
-add_button = sq.Button("Add")
-window = sq.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
-window.close()
