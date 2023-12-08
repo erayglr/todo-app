fa908b5ed86e6d509059058f00bd75787be9a173
Eray <erayguler6757@gmail.com>
2023-12-09 02:14:54 +0300

    Implement the complete and exit buttons

--- Todo/gui.py
+++ Todo/gui.py
@@ -7,10 +7,13 @@ add_button = sq.Button("Add")
 list_box = sq.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sq.Button("Edit")
+complete_button = sq.Button("Complete")
+exit_button = sq.Button("Exit")
 window = sq.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 15))
 while True:
     event, values = window.read()
@@ -24,16 +27,25 @@ while True:
             functions.write_todos(todos)
             window["todos"].update(values=todos)
         case "Edit":
-                todo_to_edit = values["todos"][0]
-                new_todo = values["todo"]
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
 
-                todos = functions.get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo
-                functions.write_todos(todos)
-                window["todos"].update(values=todos)
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case sq.WINDOW_CLOSED:
             break
 window.close()
